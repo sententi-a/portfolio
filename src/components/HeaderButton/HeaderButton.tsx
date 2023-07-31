64407546f81ad9b45487330171ac896ae35a04c1
HyejiYeom <kristi8041@gmail.com>
2023-07-31 23:00:03 +0900

    [Style] HeaderButton 선택되었을 때 스타일 설정

--- src/components/HeaderButton/HeaderButton.tsx
+++ src/components/HeaderButton/HeaderButton.tsx
@@ -8,10 +8,14 @@ interface IHeaderButtonProps {
 }
 
 export default function HeaderButton({ name, selected }: IHeaderButtonProps) {
+  // get URL anchor ... 생각해보니 SSG인데 어떻게 다르게 렌더링 ?
+
   return (
     // scroll false => 페이지 이동시 스크롤을 맨 위로 올리는 게 Link 컴포넌트의 기본 동작
     <Link href={`#${name}`} scroll={false}>
-      <button className={styles.button}>{name}</button>
+      <button className={`${styles.button} ${selected ? styles.active : ""}`}>
+        {name}
+      </button>
     </Link>
   );
 }
