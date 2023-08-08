7bc1597ff5bc69e664f7d5bb706a0d81bd6dfccc
HyejiYeom <kristi8041@gmail.com>
2023-08-08 15:09:17 +0900

    [Refactor] HeaderButton 컴포넌트를 Header 컴포넌트에 통합

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,29 @@
-import { HeaderButton } from "@components/HeaderButton";
 import Link from "next/link";
 import styles from "./Header.module.css";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const navs = ["ABOUT", "PROJECTS", "CONTACT"];
+  const [active, setActive] = useState("");
+
+  // TODO: 작동 안되므로 수정
+  // Change list elem style based on URL hash
+  useEffect(() => {
+    const handleHashChange = () => {
+      const anchor = window.location.hash.substring(1);
+      setActive(anchor);
+      console.log("hash changed");
+    };
+
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
   return (
     <header id={styles.header}>
       {/* Home Logo */}
@@ -12,11 +33,19 @@ export default function Header() {
 
       {/* Header Buttons */}
       <nav>
-        <div style={{ display: "flex", gap: "2vw" }}>
-          <HeaderButton name={"ABOUT"} />
-          <HeaderButton name={"PROJECTS"} />
-          <HeaderButton name={"CONTACT"} />
-        </div>
+        <ul id={styles.ul}>
+          {navs.map((name, i) => (
+            <Link href={`#${name}`} scroll={false} key={i}>
+              <li
+                className={`${styles.button} ${
+                  active === name ? styles.active : ""
+                }`}
+              >
+                {name}
+              </li>
+            </Link>
+          ))}
+        </ul>
       </nav>
     </header>
   );
