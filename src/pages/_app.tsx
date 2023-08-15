d053c8a3df29cdac8e9b8d96687c64006332ac5f
HyejiYeom <kristi8041@gmail.com>
2023-08-15 16:57:53 +0900

    [Chore] SEO를 위한 메타데이터 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "@styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>yeomhyeji | portfolio</title>
+        <meta
+          name="description"
+          content="YeomHyeji is a front-end developer. You wanna know about her?"
+        />
+        <meta property="og:title" content="YeomHyeji" />
+        <meta property="og:description" content="Check out her works!" />
+        <meta property="og:image" content="" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
