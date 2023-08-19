25640721d65be370507e12e48ee4d863207e1392
HyejiYeom <kristi8041@gmail.com>
2023-08-19 16:31:20 +0900

    [Refactor] useFullPageScroll 훅 함수 여러 개로 분리

--- src/hooks/useFullPageScroll.tsx
+++ src/hooks/useFullPageScroll.tsx
@@ -1,60 +1,69 @@
-// components/ScrollableContainer.js
-import { useEffect } from "react";
+import { useEffect, useCallback, useRef } from "react";
 
 export default function useFullPageScroll() {
-  let isScrolling = false;
+  const timeoutRef = useRef<number | null>(null);
+  const currentSectionIndex = useRef<number>(0);
 
-  useEffect(() => {
+  /**
+   * 휠 방향에 따라 스크롤 up or down 해주는 함수
+   * @param event eventlistener를 통해 전달되는 휠 이벤트
+   */
+  const fullPageScroll = useCallback((event: WheelEvent) => {
     const sections = document.querySelectorAll(
       ".section"
     ) as NodeListOf<HTMLElement>;
     const sectionCount = sections.length;
-    let currentSectionIndex = 0;
 
-    const handleScroll = (event: any) => {
-      event.preventDefault();
-
-      // fire event just once
-      if (isScrolling) {
+    // wheel down : page up
+    if (event.deltaY < 0) {
+      if (currentSectionIndex.current > 0) {
+        currentSectionIndex.current--;
+      } else {
         return;
       }
+    }
+    // wheel up : page down
+    else {
+      if (currentSectionIndex.current < sectionCount - 1) {
+        currentSectionIndex.current++;
+      } else {
+        return;
+      }
+    }
 
-      isScrolling = true;
+    const targetSection = sections[currentSectionIndex.current];
 
-      setTimeout(() => {
-        isScrolling = false;
+    if (targetSection) {
+      const topPosition = targetSection.offsetTop;
+      window.scrollTo({ top: topPosition, behavior: "smooth" });
+    }
+  }, []);
+
+  /**
+   * 휠 이벤트 발생시 fullPageScroll를 호출하고, 휠 이벤트에 디바운스 적용
+   * @param event eventlistener를 통해 전달되는 휠 이벤트
+   */
+  const handleWheelEvent = (event: WheelEvent) => {
+    event.preventDefault();
+
+    if (!timeoutRef.current) {
+      fullPageScroll(event);
+      timeoutRef.current = window.setTimeout(() => {
+        timeoutRef.current = null;
       }, 1300);
+    }
+  };
 
-      // wheel down: move to next section
-      if (event.wheelDelta < 0) {
-        if (currentSectionIndex < sectionCount - 1) {
-          currentSectionIndex++;
-        } else {
-          return;
-        }
-      }
-
-      // wheel up: move to previous section
-      else {
-        if (currentSectionIndex > 0) {
-          currentSectionIndex--;
-        } else {
-          return;
-        }
-      }
-
-      const targetSection = sections[currentSectionIndex];
-
-      if (targetSection) {
-        const topPosition = targetSection.offsetTop;
-        window.scrollTo({ top: topPosition, behavior: "smooth" });
-      }
-    };
-
-    window.addEventListener("wheel", handleScroll, { passive: false });
+  useEffect(() => {
+    window.addEventListener("wheel", handleWheelEvent, {
+      passive: false,
+    });
 
     return () => {
-      window.removeEventListener("wheel", handleScroll);
+      window.removeEventListener("wheel", handleWheelEvent);
+      if (timeoutRef.current) {
+        window.clearTimeout(timeoutRef.current);
+      }
     };
-  }, []);
+  }, [currentSectionIndex]);
 }
