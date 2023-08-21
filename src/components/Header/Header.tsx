ce631cd0cf28fa2686a5bf96cb198ca1e929ef2f
HyejiYeom <kristi8041@gmail.com>
2023-08-21 15:22:13 +0900

    [Style] 로고 추가 후 Header 컴포넌트 display 스타일, id 네이밍 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Header.module.css";
 import { IHeaderProps } from "@customtypes/components";
 
@@ -6,6 +7,10 @@ export default function Header({ active, setActive, index }: IHeaderProps) {
   //TODO: hook으로 따로 빼기
   const navs = ["HOME", "ABOUT", "PROJECTS", "CONTACT"];
 
+  /**
+   * 파라미터에 해당하는 name을 클래스 이름으로 하는 div로 이동하고, 현재 섹션의 index를 조정
+   * @param name 이동하고자 하는 div의 클래스 이름
+   */
   const handleOnClick = (name: string) => {
     const targetSection = document.getElementById(name);
     window.scrollTo({ top: targetSection?.offsetTop, behavior: "smooth" });
@@ -18,27 +23,27 @@ export default function Header({ active, setActive, index }: IHeaderProps) {
     <header id={styles.header}>
       {/* Home Logo */}
       <Link href="/" scroll={false}>
-        <div id={styles.logo}>HYEJIYEOM</div>
+        <div id={styles.logoWrapper}>
+          <Image
+            src="/logo.svg"
+            alt="YHJ Logo"
+            className={styles.vercelLogo}
+            width={20}
+            height={20}
+            priority
+          />
+          <div id={styles.logo}>YHJ</div>
+        </div>
       </Link>
 
       {/* Header Buttons */}
       <nav>
-        <ul id={styles.ul}>
+        <ul id={styles.navButtons}>
           {navs.map((name, i) => {
             // HOME은 navigation bar에서 제외
             if (name === "HOME") return;
 
             return (
-              // <Link href={`#${name}`} scroll={false} key={i}>
-              //   <li
-              //     className={`${styles.button} ${
-              //       active === name ? styles.active : ""
-              //     }`}
-              //     onClick={() => setActive(name)}
-              //   >
-              //     {name}
-              //   </li>
-              // </Link>
               <li key={`nav-${i}`}>
                 <button
                   className={`${styles.button} ${
