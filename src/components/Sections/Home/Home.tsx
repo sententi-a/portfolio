107a88165a22acbb96052f7d3c2c87b3817fe053
HyejiYeom <kristi8041@gmail.com>
2023-08-31 23:57:00 +0900

    [Style] Montserrat 폰트 추가 및 Home 섹션 내용 추가

--- src/components/Sections/Home/Home.tsx
+++ src/components/Sections/Home/Home.tsx
@@ -1,9 +1,19 @@
+import Spline from "@splinetool/react-spline";
 import { SectionWrapper } from "@components/SectionWrapper";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <SectionWrapper name="HOME">
-      <div></div>
+      <Spline
+        id={styles.bubble}
+        scene="https://prod.spline.design/vO4hp3BNgVR0sU35/scene.splinecode"
+      />
+      {/* //TODO: font 스타일 globals.css에 정리*/}
+      <div id={styles.t3}>A front-end developer based on Seoul, Korea</div>
+      <div id={styles.h1}>YeomHyeji</div>
+      <br />
+      <div id={styles.h2}> Wanna know about me?</div>
     </SectionWrapper>
   );
 }
