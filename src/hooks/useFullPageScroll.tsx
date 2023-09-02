162a1dc1f52f27a1255e41c41e519c7937f25972
HyejiYeom <kristi8041@gmail.com>
2023-09-02 15:42:38 +0900

    [Refactor] useFullPageScroll 최적화, 새로고침시 스크롤 위치 리셋
    fullPageScroll이 불릴 때마다 초기화되던 sections를 mount 후 한 번만 초기화 될 수 있게 수정
    history.scrollRestoration을 사용해 새로고침 후 스크롤 위치가 페이지의 top이 되도록 추가

--- src/hooks/useFullPageScroll.tsx
+++ src/hooks/useFullPageScroll.tsx
@@ -1,60 +1,65 @@
-import { useEffect, useCallback, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function useFullPageScroll() {
   const timeoutRef = useRef<number | null>(null);
   const currentSectionIndex = useRef<number>(0);
 
-  /**
-   * 휠 방향에 따라 스크롤 up or down 해주는 함수
-   * @param event eventlistener를 통해 전달되는 휠 이벤트
-   */
-  const fullPageScroll = useCallback((event: WheelEvent) => {
+  useEffect(() => {
+    /* Show top of the page on refresh */
+    if (history.scrollRestoration) {
+      history.scrollRestoration = "manual";
+    }
+
     const sections = document.querySelectorAll(
       ".section"
     ) as NodeListOf<HTMLElement>;
     const sectionCount = sections.length;
 
-    // wheel down : page up
-    if (event.deltaY < 0) {
-      if (currentSectionIndex.current > 0) {
-        currentSectionIndex.current--;
-      } else {
-        return;
+    /**
+     * 휠 방향에 따라 스크롤 up or down 해주는 함수
+     * @param event eventlistener를 통해 전달되는 휠 이벤트
+     */
+    const fullPageScroll = (event: WheelEvent) => {
+      // wheel down : page up
+      if (event.deltaY < 0) {
+        if (currentSectionIndex.current > 0) {
+          currentSectionIndex.current--;
+        } else {
+          return;
+        }
       }
-    }
-    // wheel up : page down
-    else {
-      if (currentSectionIndex.current < sectionCount - 1) {
-        currentSectionIndex.current++;
-      } else {
-        return;
+      // wheel up : page down
+      else {
+        if (currentSectionIndex.current < sectionCount - 1) {
+          currentSectionIndex.current++;
+        } else {
+          return;
+        }
       }
-    }
 
-    const targetSection = sections[currentSectionIndex.current];
+      const targetSection = sections[currentSectionIndex.current];
 
-    if (targetSection) {
-      const topPosition = targetSection.offsetTop;
-      window.scrollTo({ top: topPosition, behavior: "smooth" });
-    }
-  }, []);
+      if (targetSection) {
+        const topPosition = targetSection.offsetTop;
+        window.scrollTo({ top: topPosition, behavior: "smooth" });
+      }
+    };
 
-  /**
-   * 휠 이벤트 발생시 fullPageScroll를 호출하고, 휠 이벤트에 디바운스 적용
-   * @param event eventlistener를 통해 전달되는 휠 이벤트
-   */
-  const handleWheelEvent = (event: WheelEvent) => {
-    event.preventDefault();
+    /**
+     * 휠 이벤트 발생시 fullPageScroll를 호출하고, 휠 이벤트에 디바운스 적용
+     * @param event eventlistener를 통해 전달되는 휠 이벤트
+     */
+    const handleWheelEvent = (event: WheelEvent) => {
+      event.preventDefault();
 
-    if (!timeoutRef.current) {
-      fullPageScroll(event);
-      timeoutRef.current = window.setTimeout(() => {
-        timeoutRef.current = null;
-      }, 1300);
-    }
-  };
+      if (!timeoutRef.current) {
+        fullPageScroll(event);
+        timeoutRef.current = window.setTimeout(() => {
+          timeoutRef.current = null;
+        }, 1300);
+      }
+    };
 
-  useEffect(() => {
     window.addEventListener("wheel", handleWheelEvent, {
       passive: false,
     });
