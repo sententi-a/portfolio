9f0674f10ce8e4b5b809ba1d7af361d053e07384
HyejiYeom <kristi8041@gmail.com>
2023-09-02 16:53:51 +0900

    [Feat] New -> Index 페이지 교체

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
+import { db } from "@firebase/firebase";
+import { collection, query, getDocs } from "firebase/firestore";
+import { ProjectsType } from "@customtypes/index";
 import styles from "@styles/index.module.css";
+import { Header } from "@components/Header";
+import { Home, About, Projects, Contact } from "@components/Sections";
+import useActiveSectionOnWheel from "@hooks/useActiveSectionOnWheel";
+import useFullPageScroll from "@hooks/useFullPageScroll";
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    //*-- Get all project documents from firestore --*//
+    const q = query(collection(db, "projects"));
+
+    const result = await getDocs(q);
+    const projects = result.docs.map((doc) => doc.data());
+
+    return { props: { projects } };
+  } catch (e) {
+    console.error(e);
+    const projects = null;
+    return { props: { projects } };
+  }
+}
+
+export default function Index({ projects }: { projects: ProjectsType }) {
+  //*-- Change hash in URL on scroll && Get active section --*//
+  const { activeSection, setActiveSection } = useActiveSectionOnWheel();
+  const { currentSectionIndex } = useFullPageScroll();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Header
+        active={activeSection}
+        setActive={setActiveSection}
+        index={currentSectionIndex}
+      />
+      <Home />
+      <About />
+      <Projects projects={projects} />
+      <Contact />
+    </>
   );
 }
