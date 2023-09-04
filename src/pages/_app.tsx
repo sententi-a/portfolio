67060bab35e5a3b3365e5d318a0ee919fc08b72e
HyejiYeom <kristi8041@gmail.com>
2023-09-04 19:38:10 +0900

    [Feat] favicon 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>yeomhyeji | portfolio</title>
+        <link rel="icon" href="/logo.svg"></link>
         <meta
           name="description"
           content="YeomHyeji is a front-end developer. You wanna know about her?"
