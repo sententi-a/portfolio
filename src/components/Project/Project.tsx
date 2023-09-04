274d6dc47539f6dad8be7ba3e074aa3fea402b4b
HyejiYeom <kristi8041@gmail.com>
2023-09-04 20:45:27 +0900

    [Style] Project 컴포넌트 레이아웃 및 스타일링 변경

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,14 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
 import { SectionWrapper } from "@components/SectionWrapper";
 import { Project } from "@customtypes/index";
+import styles from "./Project.module.css";
+import githubImg from "@assets/github-mark.svg";
 
 export default function Project({ project }: { project: Project }) {
+  console.log(project);
   return (
     <SectionWrapper name="PROJECTS">
-      <div>{project.title}</div>
-      <div>{project.description}</div>
-      <div>{project.role}</div>
-      <div>{project.stacks}</div>
-      <div>{project.github}</div>
+      <div className={styles.wrapper}>
+        <div className={styles.row}>
+          <div className={styles.title}>{project.title}</div>
+          <Link href={project.github} target="__blank">
+            <Image src={githubImg} width={30} height={30} alt="github"></Image>
+          </Link>
+        </div>
+        <div className={styles.description}>{project.description}</div>
+        <div className={styles.role}>{project.role}</div>
+        <div className={styles.images}></div>
+        <div className={styles.stacks}>
+          {project.stacks.map((stack) => (
+            <div className={styles.stack}>{stack}</div>
+          ))}
+        </div>
+      </div>
     </SectionWrapper>
   );
 }
